b24af6a81ba03ad482affd8f5189a6c1d1069626
Mukesh Ramanna <mukesh94910@gmail.com>
2024-08-19 10:18:50 +0530

    Drag and drop added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import End from './End';
 import Info from './Info';
 import BioData from './BioData';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import DragItem from './DragItem';
+import DropZone from './DropZone';
 
+const App = () => {
+  const [droppedItems, setDroppedItems] = useState([]);
 
-function App() {
+  const handleDrop = (item) => {
+    setDroppedItems((prevItems) => [...prevItems, item]);
+  };
+
+  const handleRemoveItem = (index) => {
+    const updatedItems = [...droppedItems];
+    updatedItems.splice(index, 1);
+    setDroppedItems(updatedItems);
+  };
+
   return (
     <Router>
-      <Navbar />
-      <div className="container">
+      <div>
+        {/* Navbar for navigation */}
+        <Navbar />
+
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/end" element={<End />} />
           <Route path="/info" element={<Info />} />
-          <Route path="/BioData" element={<BioData />} />
+          <Route path="/biodata" element={<BioData />} />
+          {/* Default route to Home */}
+          <Route path="*" element={<Navigate to="/home" />} />
         </Routes>
+
+        {/* Drag-and-Drop functionality */}
+        <DndProvider backend={HTML5Backend}>
+          <div style={{
+            display: 'flex',
+            //justifyContent: 'center',
+            alignItems: 'center',
+            height: '65vh'
+          }}>
+            <div style={{
+              border: '4px solid #ccc',
+              padding: '20px',
+              borderRadius: '5px'
+            }}>
+              <h1>Drag and Drop Example</h1>
+              <div style={{
+                display: 'flex',
+                justifyContent: 'space-around'
+              }}>
+                <div style={{
+                  border: '1px solid #ccc',
+                  padding: '10px',
+                  borderRadius: '5px'
+                }}>
+                  <h2>Drag Items</h2>
+                  <DragItem name="Item 1" />
+                  <DragItem name="Item 2" />
+                  <DragItem name="Item 3" />
+                </div>
+                <div style={{
+                  border: '1px solid #ccc',
+                  padding: '10px',
+                  borderRadius: '5px'
+                }}>
+                  <h2>Drop Zone</h2>
+                  <DropZone onDrop={handleDrop} />
+                  {droppedItems.map((item, index) => (
+                    <div
+                      key={index}
+                      style={{
+                        border: '1px solid #ccc',
+                        padding: '10px',
+                        borderRadius: '5px',
+                        marginTop: '10px',
+                        backgroundColor: 'lightblue',
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                      }}>
+                      <p>{item.name}</p>
+                      <button onClick={() => handleRemoveItem(index)}>
+                        Remove
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </DndProvider>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure this is included
 
 function Home() {
-  return( 
   
-  <div><h2>Home Page</h2>
-  <p>Hi jaffa how are you My name is Mukesh</p>
-  </div>
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/BioData');
+  };
+  
+  return( 
+    <div>
+      <h2>Home Page</h2>
+      <p>Hi jaffa how are you? My name is Mukesh</p>
+      <h1>Home Page</h1>
+      <button onClick={handleClick}>BioData page</button>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Dropdown Button
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="./End">EndPage</Dropdown.Item>
+          <Dropdown.Item href="./Info">Another action</Dropdown.Item>
+          <Dropdown.Item href="./BioData">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
   );
 }
 
 export default Home;
-
-
