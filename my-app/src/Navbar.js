f07f689132e1874810d9a76eac8709c823a1c884
Mukesh Ramanna <mukesh94910@gmail.com>
2024-08-19 21:42:55 +0530

    Chnaged complete layout

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -6,12 +6,20 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link className="navbar-brand" to="/">Mukesh</Link>
+        {/* Image next to the Family Tree text */}
+        <Link className="navbar-brand" to="/" style={{ display: 'flex', alignItems: 'center' }}>
+          <img 
+            src={`${process.env.PUBLIC_URL}/TreeDesign.jpg`} 
+            alt="Tree Design" 
+            style={{ width: '30px', height: '30px', marginRight: '10px' }} 
+          />
+          Family Tree
+        </Link>
         <div className="navbar-links">
-          <Link className="nav-link" to="/">Home</Link>
-          <Link className="nav-link" to="/end">End</Link>
-          <Link className="nav-link" to="/info">Info</Link>
-          <Link className="nav-link" to="/BioData">BioData</Link>
+          <Link className="nav-link" to="/">Parents</Link>
+          <Link className="nav-link" to="/end">Manjunath</Link>
+          <Link className="nav-link" to="/info">Mukesh</Link>
+          <Link className="nav-link" to="/BioData">Praveen</Link>
         </div>
       </div>
     </nav>
